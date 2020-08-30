a7b83dbc5d7a72dc799704bd406f092e64fb73df
Ringolol <createsite108@gmail.com>
2020-08-30 04:54:27 +0300

    dran-n-drop

--- src/components/DemoCanvas/DemoCanvasContainer.jsx
+++ src/components/DemoCanvas/DemoCanvasContainer.jsx
@@ -1,19 +1,21 @@
 import React, { useEffect } from 'react';
-import DemoCanvas from './DemoCanvas';
 import { getBlocksData } from '../../redux/blocks_reducer';
 import { connect } from 'react-redux';
+
+import { BodyWidget } from './components/DemoCanvas/BodyWidget';
+import { Application } from './components/DemoCanvas/Application';
 
 const DemoCanvasContainer = (props) => {
 
     // props.blocks   -  тут появяться данные с серва
 
+    var app = new Application();
+
     useEffect(() => {
         props.getBlocksData(); //  запрос
     }, [])
     
-    return (
-        <DemoCanvas />
-    );
+    return (<BodyWidget app={app} />);
 }
 
 const mapStateToProps = (state) => ({
