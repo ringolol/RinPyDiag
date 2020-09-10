f674f9e622ed2776fbece0fadce8e121f887f411
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-09-10 18:37:02 +0300

    Add authMe and refactoring

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,5 @@
 import * as axios from 'axios';
+import { errorsLog } from '../utils/logs/errorsLog';
 
 
 const baseURL = 'http://31.134.153.18/';
@@ -8,51 +9,28 @@ const instance = axios.create({
     baseURL: baseURL,
 });
 
-const errorsLog = (error) => {
-    if(error.response) { 
-        // Request made and server responded
-        console.log(error.response.data, error.response.status, error.response.headers);
-    } else if (error.request) {
-        // The request was made but no response was received
-        console.log(error.request);
-    } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log('Error', error.message);
-    }
-    return null;
-}
-
 
 export const authAPI = {
-    async login (username, password) {
-        let token = localStorage.getItem('REACT_TOKEN_AUTH') || null;
+    authMe () {
+        const token = localStorage.getItem('REACT_TOKEN_AUTH');
+        const username = localStorage.getItem('REACT_USERNAME');
+        return [token, username] || [null, null];
 
-        // если нет токена, но есть юзернейм и пасс
-        // посылаем запрос на логирование
-        if (!token && username && password) {
-            console.log('get-token')
-            return await instance.post('/api-token-auth/', {
-                username: username,
-                password: password,
-            }).then(res => {
-                localStorage.setItem('REACT_TOKEN_AUTH', res.data.token);
-                localStorage.setItem('REACT_USERNAME', username);
-                return res.data.token;
-            }).catch(function (error) {
-                return errorsLog(error);
+    },
+    async login (username, password) {
+        try {
+            const response = await instance.post('/api-token-auth/', {
+                username,
+                password,
             });
-        // если есть токен уже есть, то отдаем его
-        } else if (token) {
-            console.log('return-token')
-            return await (async () => {
-                return token
-            })();
-        // иначе ниче не делаем
-        } else {
-            return;
+            console.log('get-token');
+            localStorage.setItem('REACT_TOKEN_AUTH', response.data.token);
+            localStorage.setItem('REACT_USERNAME', username);
+            return response.data.token;
+        } catch (error) {
+            return errorsLog(error);
         }
     },
-
     logout () { 
         localStorage.removeItem('REACT_TOKEN_AUTH');
         localStorage.removeItem('REACT_USERNAME')
@@ -60,8 +38,10 @@ export const authAPI = {
 };
 
 export const sendFileAPI = {
-    sendFile(token, json) {
-        return instance.post('/diagram/api/files/', json, {
+    sendFile(token, user, name, ser) {
+        return instance.post('/diagram/api/files/', {
+            user, name, ser
+        }, {
             headers: {
                 'Authorization': `Token ${token}`,
                 'Content-Type': 'application/json'
