f97f60737ce91dc6e79fdfe8ef0ff69beff3c8d0
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-09-10 18:37:36 +0300

    Refactoring blocks_reducer

--- src/redux/blocks_reducer.ts
+++ src/redux/blocks_reducer.ts
@@ -1,4 +1,5 @@
-import { blocksAPI, filesAPI, authAPI, sendFileAPI } from "../api/api";
+import { getToken, getUsername } from './auth_selectors';
+import { blocksAPI, filesAPI, sendFileAPI } from "../api/api";
 import { BloksType, FilesType } from "../types/types";
 
 const GET_BLOCKS = 'blocks/GET_BLOCKS';
@@ -6,17 +7,13 @@ const GET_FILES = 'blocks/GET_FILES';
 const SET_FILE = 'blocks/SET_FILE';
 const IS_LOADED = 'blocks/IS_LOADED';
 const SET_FILE_NAME = 'blocks/SET_TEXT';
-const SET_USERNAME = 'blocks/SET_USERNAME';
-const SET_PASSWORD = 'blocks/SET_PASSWORD';
 
 
 let initialState = {
     blocks: [] as Array<BloksType>,
     files: [] as Array<FilesType>,
     isLoaded: false as boolean,
-    filename: '' as string,
-    username: '' as string,
-    password: '' as string
+    filename: '' as string
 };
 
 export type InitialStateType = typeof initialState;
@@ -54,18 +51,6 @@ const blocksReducer = (state = initialState, action: any): InitialStateType => {
                 filename: action.filename,
             }
         }
-        case SET_USERNAME: {
-            return {
-                ...state,
-                username: action.username,
-            }
-        }
-        case SET_PASSWORD: {
-            return {
-                ...state,
-                password: action.password,
-            }
-        }
         default:
             return state;
     }  
@@ -96,70 +81,36 @@ export type SetFileNameActionType = {
     filename: string
 }
 export const setFileName = (filename: string): SetFileNameActionType => ({ type: SET_FILE_NAME, filename });
-export type SetUserNameActionType = {
-    type: typeof SET_USERNAME
-    username: string
-}
-export const setUserName = (username: string): SetUserNameActionType => ({ type: SET_USERNAME, username });
-export type SetPasswordActionType = {
-    type: typeof SET_PASSWORD
-    password: string
-}
-export const setPassword = (password: string): SetPasswordActionType => ({ type: SET_PASSWORD, password });
 
 
-export const onLogIn = (username: string, password: string) =>  (dispatch: any) => {
-    // отрисовываем логин форму и тп
-    dispatch(setIsLoaded(true));
 
-    // логин по имени пользователя и паролю
-    authAPI.login(username, password).then(token => {
-        console.log(token);
-        loadContent(token, dispatch);
-    })
-}
-
-const loadContent = (token: string, dispatch: any) => {
-    if(!token) {
-        return;
-    }
-    
+export const downloadContent = (token: string | null) => (dispatch: any) => {
     const blocks = blocksAPI.getBlocks(token);
     const files = filesAPI.getFiles(token);
 
-    Promise.all([blocks, files])
-    .then(resolve => {
-        console.log(resolve);
-        if(resolve[0] && resolve[0].status === 200) {
-            dispatch(setBlocks(resolve[0].data));
-        }
-        if(resolve[1] && resolve[1].status === 200) {
-            dispatch(setFiles(resolve[1].data));
-        }
-        dispatch(setIsLoaded(true));
+    Promise.all([blocks, files]).then(resolve => {
+        const [blocks, files] = resolve;
+        if(blocks.status === 200 && files.status === 200) {
+            dispatch(setBlocks(blocks.data));
+            dispatch(setFiles(files.data));
+            dispatch(setIsLoaded(true));
+            console.log(blocks, files) // delete
+        } else {
+            console.warn()
+        } 
     })
 }
 
-export const sendFile = (filename: string, ser: string) => async (dispatch: any) => {
-    let token = localStorage.getItem('REACT_TOKEN_AUTH') || null;
-    let username = localStorage.getItem('REACT_USERNAME') || null;
-    let json = {
-        user: username,
-        name: filename,
-        ser: ser
-    }
-
-    const response = await sendFileAPI.sendFile(token, json);
+export const sendFile = (filename: string, ser: string) => async (dispatch: any, getState: any) => {
+    const state = getState();
+    const token = getToken(state);
+    const username = getUsername(state);
+    const response = await sendFileAPI.sendFile(token, username, filename, ser);
     if (response && response.status === 201) {
         dispatch(setFile(response.data));
     }
 }
 
-export const onLogOut = () => (dispatch: any) => {
-    authAPI.logout()
-    dispatch(setBlocks([]));
-    dispatch(setFiles([]));
-}
 
 
 
