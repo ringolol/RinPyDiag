009b252b97134c4577e7bdf2eb7dac8d49af09dd
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-09-11 23:38:22 +0300

    Refactoring

--- src/components/Diagram/Tray/TrayWidget.tsx
+++ src/components/Diagram/Tray/TrayWidget.tsx
@@ -2,16 +2,15 @@ import * as React from 'react';
 import styled from '@emotion/styled';
 
 
+export class TrayWidget extends React.Component {
+	render() {
+		return <Tray>{this.props.children}</Tray>;
+	}
+}
+
 export const Tray = styled.div`
 	min-width: 200px;
 	background: rgb(20, 20, 20);
 	flex-grow: 0;
 	flex-shrink: 0;
 `;
-
-
-export class TrayWidget extends React.Component {
-	render() {
-		return <Tray>{this.props.children}</Tray>;
-	}
-}
